9b940855a8197f80eb02202b3547603264d25a5b
sohag sagar <sagorsohag29@gmail.com>
2022-06-07 13:40:56 +0600

    reset password system added

--- src/Components/Pages/Login/PasswordResetModal.js
+++ src/Components/Pages/Login/PasswordResetModal.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import auth from '../../../FirebaseAPI/firebase.init';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import Loading from '../../Shared/Loading';
+import { MdError } from 'react-icons/md';
+import toast from 'react-hot-toast';
 
-const PasswordResetModal = ({setModalStatus}) => {
+const PasswordResetModal = ({ setModalStatus }) => {
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const [resetError, setResetError] = useState('')
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
+        auth
+    );
+
+    const onSubmit = data => {
+        console.log(data);
+        sendPasswordResetEmail(data?.email)
+        if (sending) {
+            return <Loading />
+        }
+        if (error) {
+            setResetError(error?.code.split('/')[1]);
+        }
+        else {
+            toast.success('Reset Link send to your email');
+            setResetError('')
+        }
+    }
     return (
         <div>
             <input type="checkbox" id="resetPasswordModal" className="modal-toggle" />
@@ -11,6 +34,14 @@ const PasswordResetModal = ({setModalStatus}) => {
                 <div className="modal-box">
                     <label htmlFor="resetPasswordModal" className="btn btn-sm  absolute right-2 top-2">✕</label>
                     <h1 className='text-center text-xl text-accent font-semibold mb-2'>Reset Your password</h1><hr />
+
+                    {/* SHOWING LOGIN ERRORS */}
+                    {
+                        resetError &&
+                        <div className="alert  shadow-md">
+                            <span className='mx-auto font-semibold text-[14px] text-red-600'><MdError />{resetError}</span>
+                        </div>
+                    }
 
                     <form onSubmit={handleSubmit(onSubmit)} className="uppercase font-semibold mt-5 ">
 
@@ -41,7 +72,7 @@ const PasswordResetModal = ({setModalStatus}) => {
                         </div>
 
                         <div className="form-control w-1/2 mx-auto">
-                            <button onClick={()=>setModalStatus(false)} type='submit' className="btn drop-shadow-xl hover:bg-primary hover:text-secondary btn-sm bg-secondary border-0  text-primary font-semibold rounded-full px-5 h-10">RESET NOW</button>
+                            <button type='submit' className="btn drop-shadow-xl hover:bg-primary hover:text-secondary btn-sm bg-secondary border-0  text-primary font-semibold rounded-full px-5 h-10">RESET NOW</button>
 
                         </div>
                     </form>
